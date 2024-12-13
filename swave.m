91382da6173d8e5b48b43206e83df64f2cf961dc
Qiaoping He <75305603+sevenjava@users.noreply.github.com>
2024-12-13 21:13:12 +0800

    Update swave.m

--- swave.m
+++ swave.m
@@ -1,6 +1,5 @@
 function Lfimg = swave(X,h,w)
-%UNTITLED3 此处提供此函数的摘要
-%   此处提供详细说明
+%UNT
 X1=imresize(X,[2*h 2*w]);
 X1=rgb2gray(X1);
 [c,s] =wavedec2(X1,1,'haar');
@@ -8,5 +7,5 @@ X1=rgb2gray(X1);
 A3=appcoef2(c,s,'haar',1);
 A3img=wcodemat(A3,255,'mat',1);
 Lfimg=abs(A3img-255)./255;
-% % % Lfimg=abs(A3img)./255; %%%%%影响oxford5k的性能，只有0.76差0.04
+
 end
